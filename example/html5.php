549b12b53c109ecf2a08e549f99a1b655193092b
Enjoys <deadly.info@gmail.com>
2022-04-28 11:44:17 +0300

    edited example

---
+++ example/html5.php
@@ -0,0 +1,35 @@
+<?php
+
+use Enjoys\Forms\AttributeFactory;
+use Enjoys\Forms\Form;
+use Enjoys\Forms\Renderer\Bootstrap4\Bootstrap4Renderer;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$form = new Form();
+
+$form->header('HTML5 inputs');
+$form->color('color1', 'Color');
+$form->date('date1', 'Date');
+$form->datetime('datetime1', 'DateTime');
+$form->datetimelocal('datetimelocal1', 'DateTime Local');
+$form->email('email1', 'E-mail');
+$form->email('email2', 'E-mail / multiple')->setAttr(AttributeFactory::create('multiple'))->setDescription('Список e-mail вводится через запятую');
+$form->month('month1', 'Month');
+$form->number('number1', 'Number');
+$form->range('range1', 'Range');
+$form->search('search1', 'Search');
+$form->tel('tel1', 'Tel');
+$form->time('time1', 'Time');
+$form->url('url1', 'Url');
+$form->week('week1', 'Week');
+
+$renderer = new Bootstrap4Renderer();
+$renderer->setForm($form);
+
+echo include __DIR__.'/.assets.php';
+echo sprintf('<div class="container">%s</div>', $renderer->output());
+
+
+
+
--- example/switch.php
+++ example/switch.php
@@ -28,15 +28,7 @@ $renderer->setOptions([
     'switch' => ['active', 'many']
 ]);
 
-?>
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-      integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF"
-        crossorigin="anonymous"></script>
-
-<?php
+echo include __DIR__.'/.assets.php';
 echo sprintf('<div class="container-fluid">%s</div>', $renderer->output());
 
 
