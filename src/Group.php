7ef8f914813fb633e1747c03c6fc7c265f89c390
enjoys <deadly.info@gmail.com>
2022-06-11 20:21:01 +0300

    allow attributes for group

--- src/Group.php
+++ src/Group.php
@@ -5,29 +5,37 @@ declare(strict_types=1);
 namespace Enjoys\Forms\Renderer\Bootstrap4;
 
 use Enjoys\Forms\Element;
+use Enjoys\Forms\Renderer\Html\TypesRender\Input;
 
 class Group extends Input
 {
+    public const ATTRIBUTES_GROUP = '_group_attributes_';
+
+    public function __construct(Element $element)
+    {
+        $element->addClass('form-row');
+        parent::__construct($element);
+    }
+
     protected function bodyRender(Element $element): string
     {
-//        $element->addClass('form-group');
-//        $return = sprintf("<div%s>", $element->getAttributesString());
         $return = '';
         /** @var \Enjoys\Forms\Elements\Group $element */
         foreach ($element->getElements() as $data) {
-            $return .= "<div class='col'>";
-            $return .= Bootstrap4Renderer::createTypeRender($data, $this->getOptions())->render();
+            $data->addClass('col', self::ATTRIBUTES_GROUP);
+            $return .= sprintf("<div%s>", $data->getAttributesString(self::ATTRIBUTES_GROUP));
+            $return .= Bootstrap4Renderer::createTypeRender($data)->render();
             $return .= '</div>';
         }
-//        $return .= '</div>';
         return $return;
     }
 
     public function render(): string
     {
         return sprintf(
-            "%s\n<div class='form-row'>%s\n%s\n%s</div>",
+            "%s\n<div%s>%s\n%s\n%s</div>",
             $this->labelRender(),
+            $this->getElement()->getAttributesString(),
             $this->bodyRender($this->getElement()),
             $this->descriptionRender(),
             $this->validationRender(),
